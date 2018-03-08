9d857b2818a8a86dd71f826adfddaf58b750ba4e
smart-202-10 <smart-202-10@smart-202-10-PC>
2018-03-08 13:32:13 +0900

    [김재희] 내용삭제 및 업로드

--- mvc27team5/src/controller/StudentController.java
+++ mvc27team5/src/controller/StudentController.java
@@ -8,34 +8,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class GuestController
+ * Servlet implementation class StudentController
  */
-@WebServlet("/GuestController")
-public class GuestController extends HttpServlet {
+@WebServlet("/addStudentjjdev")
+public class StudentController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GuestController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
+  
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
---
+++ mvc27team5/src/model/Student.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Student {
+	private int studentNo;
+	private String studentId;
+	private String studentPw;
+	
+	
+}
